6cf8847431a64244bda629024396ef83c57505ef
lukasznoroff <lukasznoroff@gmail.com>
2022-05-27 18:25:13 +0200

    Update images.js

--- src/images.js
+++ src/images.js
@@ -6,4 +6,5 @@ import room5 from "./images/room5.jpg";
 import room6 from "./images/room6.jpg";
 import room7 from "./images/room7.jpg";
 
-export default [room1, room2, room3, room4, room5, room6, room7];
+const images = [room1, room2, room3, room4, room5, room6, room7];
+export default images;
